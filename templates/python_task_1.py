8c9b9d1adb56281a52020dcb9dd924dfe83ece0e
KajalJuleesh <153415365+KajalJuleesh@users.noreply.github.com>
2023-12-11 18:20:17 +0530

    python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -13,10 +13,16 @@ def generate_car_matrix(df)->pd.DataFrame:
                           where 'id_1' and 'id_2' are used as indices and columns respectively.
     """
     # Write your logic here
+    dataset = df.copy()
+    car_matrix = dataset.pivot(index='id_1', columns='id_2', values='car').fillna(0)
 
-    return df
+    # Set diagonal to 0
+    car_matrix.values[[range(len(car_matrix))]*2] = 0
+
+    return car_matrix
 
 
+import numpy as np
 def get_type_count(df)->dict:
     """
     Categorizes 'car' values into types and returns a dictionary of counts.
@@ -28,8 +34,13 @@ def get_type_count(df)->dict:
         dict: A dictionary with car types as keys and their counts as values.
     """
     # Write your logic here
+    df['car_type'] = pd.cut(df['car'], bins=[-float('inf'), 15, 25, float('inf')],
+                            labels=['low', 'medium', 'high'], right=False)
+    type_counts = df['car_type'].value_counts().to_dict()
+    sorted_type_counts = dict(sorted(type_counts.items()))
 
-    return dict()
+    return sorted_type_counts
+
 
 
 def get_bus_indexes(df)->list:
@@ -43,8 +54,17 @@ def get_bus_indexes(df)->list:
         list: List of indexes where 'bus' values exceed twice the mean.
     """
     # Write your logic here
+   
+    data = pd.read_csv(df)
+    mean_column_bus = data['bus'].mean()
+    index_list = []
+    for val in data.bus:
+        if val > 2*mean_column_bus:
+            index_list.append(val)
+            
+    index_list.sort()
+    return index_list
 
-    return list()
 
 
 def filter_routes(df)->list:
@@ -58,8 +78,16 @@ def filter_routes(df)->list:
         list: List of route names with average 'truck' values greater than 7.
     """
     # Write your logic here
+    data = pd.read_csv(df)
+    avg_column_truck = data['bus'].mean()
+    index_list = []
+    for val in data.route:
+        if val > 7*avg_column_truck:
+            index_list.append(val)
+            
+    index_list.sort()
+    return index_list
 
-    return list()
 
 
 def multiply_matrix(matrix)->pd.DataFrame:
@@ -73,6 +101,15 @@ def multiply_matrix(matrix)->pd.DataFrame:
         pandas.DataFrame: Modified matrix with values multiplied based on custom conditions.
     """
     # Write your logic here
+    modified_matrix = matrix.copy()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] > 20:
+                modified_matrix[i][j] = matrix[i][j] * 0.75
+            else:
+                modified_matrix[i][j] = matrix[i][j] * 1.25
+
+    return modified_matrix
 
     return matrix
 
@@ -88,5 +125,12 @@ def time_check(df)->pd.Series:
         pd.Series: return a boolean series
     """
     # Write your logic here
+    df['start_timestamp'] = pd.to_datetime(df['startDay'] + ' ' + df['startTime'])
+    df['end_timestamp'] = pd.to_datetime(df['endDay'] + ' ' + df['endTime'])
+    df['duration'] = df['end_timestamp'] - df['start_timestamp']
+    completeness_check = df.groupby(['id', 'id_2'])['duration'].apply(
+        lambda x: (x.max() - x.min()) >= pd.Timedelta(days=7, hours=23, minutes=59, seconds=59)
+    )
 
+    return completeness_check
     return pd.Series()
